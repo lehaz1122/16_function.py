db71d7e7c1d6812ace500ff6a4148d964837d114
lehaz1122 <lehadkhan1122@gmail.com>
2025-08-27 12:32:48 +0500

    Add files via upload

---
+++ 16_function.py
@@ -0,0 +1,21 @@
+# print("Hello lihaz khan")
+
+
+
+# def greet():
+#     print("hi, how are you?")
+
+# # greet()    
+
+
+# print("hello, waqas khan")
+
+name = input("enter your name")
+def college(name, gender, age):
+    if gender == "male":
+         print(f"hello,{name}, yor gender is {gender}, and age is {age}")
+    else:
+        print(f"hello, {name}, your gender is female")
+    
+    
+college("waqas", "female", 22 )      
